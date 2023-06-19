5cdb27d29ed33413a2b38a05ffdfb1aad5c47dfa
juanibarguen <h===>
2023-06-19 18:45:46 -0300

    se creo la interfaz para los productos agregados al carrito, tambien se actualizaron los datos del array a productos reales

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.interface';
+import { ProductInCart } from 'src/app/models/productInCart.interface';
 import { ProductsService } from 'src/app/service/products.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { ProductsService } from 'src/app/service/products.service';
 export class HomeComponent implements OnInit {
   // Creamos un array de elementos de tipo Producto para agregar los elementos que vengan del servicio
   products: Product[] = [];
-  productsCart: Product[] = [];
+  productsCart: ProductInCart[] = [];
   constructor(private productService: ProductsService) { }
 
   ngOnInit() {
@@ -21,17 +22,34 @@ export class HomeComponent implements OnInit {
     
   }
 
-
+  
   addToCart(id: number) {
-
     const productToAdd = this.products.find(product => product.id === id);
-    if (productToAdd) {
-      this.productsCart.push(productToAdd);
+  
+    if (productToAdd && productToAdd.stock > 0) {
+      const existingProduct = this.productsCart.find(product => product.id === id);
+  
+      const newProduct: ProductInCart = { ...productToAdd, cantidad: 1 };
+      
+      if (existingProduct) {
+        existingProduct.cantidad += 1;
+      } else {
+        this.productsCart.push(newProduct);
+      }
+  
+      productToAdd.stock -= 1;
     }
+  
     console.log(this.productsCart);
-    
   }
+  
 
+  deleteComponentCart(idProduct: number) {
+    const index = this.productsCart.findIndex(el => el.id === idProduct);
+    if (index !== -1) {
+      this.productsCart.splice(index, 1);
+    }
+  }
   
   
 
